ae6eab0df66cd6e55106c40a5f34cafb02df22a5
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-08-30 13:56:31 +0600

    From end ready for creation

--- BillGenerator/Controllers/HomeController.cs
+++ BillGenerator/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
+using System.Linq;
 
 namespace BillGenerator.Controllers
 {
@@ -16,7 +17,9 @@ namespace BillGenerator.Controllers
 
         public IActionResult Index()
         {
-
+            ViewBag.BillerList = new SelectList(_context.Billers, "Id", "Name");
+            ViewBag.FieldTypeList = new SelectList(_context.BillerFormFieldTypes, "Id", "Description");
+            ViewBag.DatasetList = new SelectList(_context.BillerFormDatasets, "Id", "DatasetName");
             return View();
         }
         public IActionResult Create()
@@ -26,8 +29,9 @@ namespace BillGenerator.Controllers
             ViewBag.DatasetList = new SelectList(_context.BillerFormDatasets, "Id", "DatasetName");
             BillerFormDataset billerFormDataset = new BillerFormDataset();
             billerFormDataset.BillerFormDatasetFields.Add(new BillerFormDatasetField());
-            
+
             return View(billerFormDataset);
+
         }
 
         [HttpPost]
@@ -36,6 +40,29 @@ namespace BillGenerator.Controllers
         {
             if (ModelState.IsValid)
             {
+                foreach (var (dataFields, bFDF) in from dataFields in model.BillerFormDatasetFields
+                                                   let bFDF = new BillerFormDatasetField()
+                                                   select (dataFields, bFDF))
+                {
+                    bFDF.IsMandatory = dataFields.IsMandatory;
+                    bFDF.MaxLength = dataFields.MaxLength;
+                    bFDF.Regex = dataFields.Regex;
+                    bFDF.FieldTypeId = dataFields.FieldTypeId;
+                    bFDF.CreatedBy = dataFields.CreatedBy;
+                    bFDF.CreatedAt = dataFields.CreatedAt;
+                    bFDF.UpdatedBy = dataFields.UpdatedBy;
+                    bFDF.UpdatedAt = dataFields.UpdatedAt;
+                    bFDF.DatasetId = dataFields.DatasetId;
+                    bFDF.MinValue = dataFields.MinValue;
+                    bFDF.MaxValue = dataFields.MaxValue;
+                    bFDF.FieldOrder = dataFields.FieldOrder;
+                    bFDF.FieldName = dataFields.FieldName;
+                    bFDF.FriendlyFieldName = dataFields.FriendlyFieldName;
+                    bFDF.IsActive = dataFields.IsActive;
+                    _context.BillerFormDatasetFields.Add(bFDF);
+                }
+
+                
                 _context.BillerFormDatasets.Add(model);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
